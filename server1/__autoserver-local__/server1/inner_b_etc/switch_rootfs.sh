b2be4a4860281ae7c22fdd6a13c7cb93a0e8659b
Peter H. Jin <peter@peterjin.org>
2022-11-27 19:53:48 -0600

    More files

--- server1/__autoserver-local__/server1/inner_b_etc/switch_rootfs.sh
+++ server1/__autoserver-local__/server1/inner_b_etc/switch_rootfs.sh
@@ -2,4 +2,4 @@
 set -eu
 export NEW_FSROOT_RO="$1"
 shift
-exec unshare -r -m sh -eu -c 'mount --bind -- "$NEW_FSROOT_RO" /_fsroot_ro; mount --bind /_fsroot_ro/etc /etc; echo 0 > /proc/sys/user/max_user_namespaces; exec setpriv --bounding-set=-all "$@"' _ "$@"
+exec unshare -r -m sh -eu -c 'mount --rbind -- "$NEW_FSROOT_RO" /_fsroot_ro; mount --rbind /_fsroot_ro/etc /etc; echo 0 > /proc/sys/user/max_user_namespaces; exec setpriv --bounding-set=-all "$@"' _ "$@"
