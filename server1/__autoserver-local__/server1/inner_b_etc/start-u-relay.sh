ea4e75e43ab5802cf8155467e2427729e5623117
Peter H. Jin <peter@peterjin.org>
2022-12-24 23:14:25 -0600

    fix wrong name for wg vrf interface

--- server1/__autoserver-local__/server1/inner_b_etc/start-u-relay.sh
+++ server1/__autoserver-local__/server1/inner_b_etc/start-u-relay.sh
@@ -9,6 +9,6 @@ exec /ctr_fs1/_system/bin/ctrtool ns_open_file -o10 \
 	setpriv --reuid=matrix-synapse --regid=matrix-synapse --clear-groups \
 	env NODE_PATH=/ctr_fs2/node_js/usr/local/lib/node_modules \
 	LD_PRELOAD='/ctr_fs1/_system/bin/libsocketbox-preload.so:/usr/local/lib/socket-enhancer' \
-	SOCKET_ENHANCER_CONFIG='bdpr=16384/Dvpn_vrf0,bdpr=32768/Dvpn_vrf0' \
+	SOCKET_ENHANCER_CONFIG='bdpr=16384/Dvrf_wg0,bdpr=32768/Dvrf_wg0' \
 	SKBOX_DIRECTORY_ROOT2=/run/socketbox \
 	/ctr_fs2/node_js/usr/local/bin/node entrypoint.js '{"transparent_fd": {"host": "fe8f::2:2:1", "port": 80}, "socks_fd": {"host": "fe8f::2:2:1", "port": 81}, "prefix": "0xfd48774fd9a1810b", "dns": ["127.0.0.1"]}'
